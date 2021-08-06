24b481ae6cc7b2611590308c90bc31f7916f30d6
Slava79198015981 <slava79198015981@gmail.com>
2021-08-06 14:29:01 +0300

    Прописывает разметку, логику и стили для главной(отрисовываемой) части

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <FirstScreen />
+    <TodoList :todos="todos" @delete-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList.vue";
+import FirstScreen from "./components/FirstScreen.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
+  data() {
+    return {
+      todos: [
+        "Изучить vue",
+        "Понять, что VUE - это КРУТО!!!!",
+        "Залить в репозиторий",
+        "Понять, что он не доработан",
+        "Доработать",
+        "Перезалить!",
+      ],
+    };
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+    FirstScreen,
+  },
+  methods: {
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  min-width: 100%;
+  min-height: 100vh;
 }
 </style>

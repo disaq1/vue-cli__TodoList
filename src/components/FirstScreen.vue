2e6f44c66739711d1447c7d101a751cccfb743a9
Slava79198015981 <slava79198015981@gmail.com>
2021-08-06 14:29:32 +0300

    Прописывает разметку, логику и стили для Верхней части приложения

---
+++ src/components/FirstScreen.vue
@@ -0,0 +1,31 @@
+﻿<template>
+  <div>
+    <h1>Мини-ежедневик, всё как у всех)</h1>
+    <p>
+      Исключительно тестовый проект, не нацеленный на бла-бла-бла, а только на
+      изучение и закрепление основ работы на VUE
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  max-width: 700px;
+  width: 100%;
+  color: white;
+  background: rgb(0, 117, 133);
+  border-radius: 20px;
+  margin: 0 0 20px;
+  padding: 20px 40px;
+}
+
+div h1 {
+  font-size: 42px;
+  font-weight: 700;
+  margin: 0 0 10px;
+}
+</style>
